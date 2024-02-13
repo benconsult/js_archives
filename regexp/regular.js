0d7610241a10acb00c2a06aece4b619e72b56556
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 12:25:42 +0100

    square bracket with two strings

--- regexp/regular.js
+++ regexp/regular.js
@@ -1,7 +1,7 @@
-//square bracket - g global
-//without g, print the index of the match pattern. If g, picks all the match text in the word
-const txt = 'Milk is very good for the body. Dringing milk is not too bad unless you dring Milk too much'
-let pattern = /[Mm]ilk/g
+
+//@ strings match with |
+const txt = 'Milk and Bannana are very good for the body. Dringing milk and eating bannana is not too bad unless you dring Milk too much'
+let pattern = /[Mm]ilk|[Bb]annana/g
 let output = txt.match(pattern)
 
 console.log(output)
