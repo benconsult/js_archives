cd69d9a6a82e36be15f73d98338df8846a53a00e
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 09:53:58 +0100

    regex declaration

---
+++ regexp/reguar.js
@@ -0,0 +1,17 @@
+/*takes 2 params - pattern(text) and flag (optional)
+RegExp constructor or usinng 2 //
+gimsuy
+**/
+
+//using RegExp object
+let pattern  = 'love'
+let flag = 'gi'
+let regExp = new RegExp(pattern,flag)
+
+//without regext constructor
+
+let regexp = /love/gi
+
+
+
+
