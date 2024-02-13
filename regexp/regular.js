9745b532ae078e8b782e82350d3a8d7aeac724c2
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 13:12:21 +0100

    period plus any character one or more times

--- regexp/regular.js
+++ regexp/regular.js
@@ -1,4 +1,4 @@
-//Period - any character except new linw, a and any other character except new line
+//Period + any character one or more times
 let pattern = /[a].+/g
 const txt = 'Apple and banana are fruits'
 let output = txt.match(pattern)
