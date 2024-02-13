947d9398870f58858b18c1d8e005a0313ac79026
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 12:21:36 +0100

    bracket example alternative

--- regexp/regular.js
+++ regexp/regular.js
@@ -1,6 +1,7 @@
-//square bracket
+//square bracket - g global
+//without g, print the index of the match pattern. If g, picks all the match text in the word
 const txt = 'Milk is very good for the body. Dringing milk is not too bad unless you dring Milk too much'
-let pattern = '[Mm]ilk'
-let ouput = txt.match(pattern)
+let pattern = /[Mm]ilk/g
+let output = txt.match(pattern)
 
-console.log(ouput)
+console.log(output)
