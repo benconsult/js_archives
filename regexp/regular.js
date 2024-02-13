1604266ddd129d628768083fbf2cb6c2d3d8546e
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 10:18:09 +0100

    test method

--- regexp/regular.js
+++ regexp/regular.js
@@ -1,7 +1,7 @@
 /*takes 2 params - pattern(text) and flag (optional)
 RegExp constructor or usinng 2 //
 gimsuy
-**/
+
 
 //using RegExp object
 let pattern  = 'love'
@@ -12,6 +12,13 @@ let regExp = new RegExp(pattern,flag)
 
 let regexp = /love/gi
 
+**/
 
+//RegExp method
+//test()  - without param
+let str = 'coding is my passion'
+let pattern = /my/
+let output = pattern.test(str)
+console.log(output)
 
 
