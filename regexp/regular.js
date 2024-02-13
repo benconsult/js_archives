c1e8d20c527765ee62726afff77543f0a58e0074
ben-jy <baremu@ip-192-168-0-173.us-west-2.compute.internal>
2024-02-13 10:31:06 +0100

    match method

--- regexp/regular.js
+++ regexp/regular.js
@@ -12,8 +12,6 @@ let regExp = new RegExp(pattern,flag)
 
 let regexp = /love/gi
 
-**/
-
 //RegExp method
 //test()  - without param
 let str = 'coding is my passion'
@@ -21,4 +19,10 @@ let pattern = /my/
 let output = pattern.test(str)
 console.log(output)
 
-
+//match = all capturing groups or null is not found. Note: used with string
+//if global flag, returns all groups , else return pattern, index, input, group,
+*/
+let str = 'coding is my passion'
+let pattern = /myt/g
+let output = str.match(pattern)
+console.log(output)
